803cd33614e57bc097098d102629fd9cb5b54d1f
Timi Delad <timilehinogunlade@gmail.com>
2025-07-11 10:06:30 +0100

    modify

--- src/app/sections/BeginBtn.tsx
+++ src/app/sections/BeginBtn.tsx
@@ -1,7 +1,9 @@
+import Link from 'next/link';
+
 export default function BeginBtn() {
     return (
         <div className="flex justify-center items-center my-[60px] mx-[16px]">
-            <button className=" bg-[#3B82F6] rounded-[30px] px-4 py-3">A Simple Prayer To Begin Your Journey With Christ</button>
+            <Link href='/simpleprayer' className=" bg-[#3B82F6] rounded-[30px] px-4 py-3">A Simple Prayer To Begin Your Journey With Christ</Link>
         </div>
     )
 }
